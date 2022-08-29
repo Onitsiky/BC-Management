44be2ae7d5ac9e956d3b558acc60f2ff1455e581
Onitsiky <hei.onitsiky@gmail.com>
2022-08-29 21:58:47 +0300

    feat: add relation to model

--- src/main/java/com/example/bcmanagementapi/Model/ExpenditureCategory.java
+++ src/main/java/com/example/bcmanagementapi/Model/ExpenditureCategory.java
@@ -3,6 +3,8 @@ package com.example.bcmanagementapi.Model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Getter
@@ -10,11 +12,14 @@ import javax.persistence.*;
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
-public class ExpenditureCategory {
+public class ExpenditureCategory implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Expenditures> expendituresList;
 }

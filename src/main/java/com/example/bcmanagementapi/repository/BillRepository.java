357849c82d84db5b13cbbe0c9c46ad8c8a74a62b
Onitsiky <hei.onitsiky@gmail.com>
2022-09-11 20:52:34 +0300

    re-initialize project

--- src/main/java/com/example/bcmanagementapi/repository/BillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bcmanagementapi.repository;
-
-import com.example.bcmanagementapi.model.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillRepository extends JpaRepository<Bill, Long> {
-}
